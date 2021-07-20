1fa272bf1219b32aff2e8ce39759ea2c5b78c3b2
igor.storchak.cr <igor.storchak.cr@gmail.com>
2021-07-20 14:58:13 +0300

    Assignment 2 -> for cheking

---
+++ react-complete-guide-Assignment-2/src/components/Expense/ExpenseRenderer/ExpenseRenderer.js
@@ -0,0 +1,24 @@
+import './ExpenseRenderer.css';
+import { ExpenseItem } from '../ExpenseItem/ExpenseItem';
+import { Card } from '../../UI/Card/Card';
+import { ExpensesFilter } from '../ExpensesFilter/ExpensesFilter';
+import { useState } from 'react';
+
+export const ExpenseRenderer = props => {
+    const [filterYear, setFilterYear] = useState('');
+
+    const filterChangehandler = event => setFilterYear(event.target.value)
+
+    return (
+        <Card className="expenses">
+            <ExpensesFilter filterChangehandler={filterChangehandler} />
+            {
+                props.expenses.filter(f => filterYear ? new Date(f.date).getFullYear() === new Date(filterYear).getFullYear() : true).map((item, index) => {
+                    return (
+                        <ExpenseItem key={index} expenceItem={item} />
+                    )
+                })
+            }
+        </Card>
+    )
+}
